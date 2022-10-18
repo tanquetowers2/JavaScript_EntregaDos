04210e1ba0daf46f23e8ef501eb31f08268c98a0
tanquetowers2 <christowers.programacion@gmail.com>
2022-10-18 09:32:41 -0300

    add javascript dos

---
+++ JS/productos.js
@@ -0,0 +1,37 @@
+const productos = [{
+    id: 1,
+    foto: "/ELEMENTOS/NI_CK4280-063-1.jpg",
+    nombre: "Remera Nike gris",
+    precio: 7999
+},
+{
+    id: 2,
+    foto: "/ELEMENTOS/NI_DA4366-451-1.jpg",
+    nombre: "Remera Nike Tenis",
+    precio: 9500
+},
+{
+    id: 3,
+    foto: "/ELEMENTOS/NI_CV2982-010-1.jpg",
+    nombre: "Remera Nike negra",
+    precio: 7999
+},
+{
+    id: 4,
+    foto: "/ELEMENTOS/DD5276605-A.webp",
+    nombre: "Remera Nike Lights",
+    precio: 6999
+},
+{
+    id: 5,
+    foto: "/ELEMENTOS/NI_DH6931-102-1.jpg",
+    nombre: "Zapatilla Nike Jordan Low Naranjas",
+    precio: 70000
+},
+{
+    id: 6,
+    foto: "/ELEMENTOS/NI_DM1199-100-1.jpg",
+    nombre: "Zapatilla Nike Jordan Low bi color",
+    precio: 70000
+}
+];
---
+++ JS/script.js
@@ -0,0 +1,49 @@
+//After clase 9
+//console.log(productos);
+const carrito = [];
+let totalCarrito;
+let contenedor = document.getElementById("misprods");
+
+
+function renderizarProds(){
+    for(const producto of productos){
+        contenedor.innerHTML += `
+            <div class="card col-sm-2">
+                <img src=${producto.foto} class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">${producto.id}</h5>
+                    <p class="card-text">${producto.nombre}</p>
+                    <p class="card-text">$ ${producto.precio}</p>
+                    <button id="btn${producto.id}" class="btn btn-primary">Comprar</button>
+                </div>
+            </div>
+        `;
+    }
+
+    //EVENTOS
+    productos.forEach(producto => {
+        //evento para cada boton
+        document.getElementById(`btn${producto.id}`).addEventListener("click",function(){
+            agregarAlCarrito(producto);
+        });
+    })
+}
+
+renderizarProds();
+
+function agregarAlCarrito(productoComprado){
+    carrito.push(productoComprado);
+    console.table(carrito);
+    alert("Producto: "+productoComprado.nombre+" agregado al carrito!");
+    document.getElementById("tablabody").innerHTML += `
+        <tr>
+            <td>${productoComprado.id}</td>
+            <td>${productoComprado.nombre}</td>
+            <td>${productoComprado.precio}</td>
+        </tr>
+    `;
+    totalCarrito = carrito.reduce((acumulador,producto)=> acumulador + producto.precio,0);
+    let infoTotal = document.getElementById("total");
+    infoTotal.innerText="Total a pagar $: "+totalCarrito;
+}
+
